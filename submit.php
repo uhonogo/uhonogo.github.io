13914da987f6c5e9125a963d657e75922e7ac9bc
uhonogo <poliesternopo@gmail.com>
2017-03-20 21:39:41 +0200

    v

--- submit.php
+++ submit.php
@@ -1,23 +1,19 @@
-<?php 
-if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['message'])){
-  
-  $headers = 'From: gfdffdf \r\n'.
-             'Reply-To: poliesternopo@gmail.com\r\n'.
-              'X-Mailer: PHP/'. phpversion();
-             
-  $theme = 'Новое сообщение';
-
-  $letter = 'Данные сообщения:\r\n';
-  $letter .='Имя: '.$_POST['name'].'\r\n';
-  $letter .='Email: '.$_POST['email'].'\r\n';
-  $letter .='Телефон: '.$_POST['phone'].'\r\n';
-  $letter .='Сообщение: '.$_POST['message'].'\r\n';
-  
-  if (mail('poliesternopo@gmail.com', $theme, $letter, $headers)){
-    echo "Сообщение отправлено!";
-  } else {
-    echo "Ошибка при отправке!";
-  }
-} else {
-  echo "Ошибка: Заполните все поля!";
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'uhonogo@yandex.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: '.$_POST['name'].'</p>
+                        <p>Телефон: '.$_POST['phone'].'</p>                        
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 }
+?>
