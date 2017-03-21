c0aeeb308a836f532c0a02089bf25858b1f30b47
uhonogo <poliesternopo@gmail.com>
2017-03-21 16:12:27 +0200

    ffx

--- js/scroll.js
+++ js/scroll.js
@@ -1,2 +1,2 @@
   var contactform =  document.getElementById('contactform');
-    contactform.setAttribute('action', '//formspree.io/' + 'uhonogo' + '@' + 'yandex' + '.' + 'ru');
+    contactform.setAttribute('action', '//formspree.io/' + 'uhonogo' + '@' + 'gmail' + '.' + 'com');
