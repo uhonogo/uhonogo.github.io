d6044ecb86f25bee300a4835077b9cdc4b9405d5
uhonogo <poliesternopo@gmail.com>
2017-03-27 17:02:07 +0300

    fx

--- js/pscripts.js
+++ js/pscripts.js
@@ -1,6 +1,6 @@
-// $(document).ready(function() {
-//     $("head").append("<link rel='stylesheet' type='text/css' href='sass/style.css' />");
-// });
+$(document).ready(function() {
+    $("head").append("<link rel='stylesheet' type='text/css' href='sass/style.css' />");
+});
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
