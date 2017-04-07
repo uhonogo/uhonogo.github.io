97f037f24edbae9c207e0aa9cefb6304c84770b2
uhonogo <poliesternopo@gmail.com>
2017-04-07 16:07:12 +0300

    mfx

--- js/parallax.js
+++ js/parallax.js
@@ -2,8 +2,17 @@ $(document).ready(function(argument) {
 	$(window).scroll(function () {
 		var lr = $(this).scrollTop();
 
+		// $(".parallaxImage").css({
+		// 	transform : "translateY(-"+lr/40 +'%)',
+		// 	// '-webkit-transform:' "translateY(-"+lr/40 +'%)',
+  //  //    		'-ms-transform:' "translateY(-"+lr/40 +'%)'
+		// });
+		// $(".parallaxImage").css("-webkit-transform": "translateY(-"+lr/40 +"%)");
 		$(".parallaxImage").css({
-			transform : "translateY(-"+lr/40 +'%)'
-		});
+			transform: 'translateY(-'+lr/40 +'%)',
+			MozTransform: 'translateY(-'+lr/40 +'%)',
+			WebkitTransform: 'translateY(-'+lr/40 +'%)',
+			msTransform: 'translateY(-'+lr/40 +'%)'
+		})
 	})
 });
