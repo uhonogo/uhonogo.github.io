8e0a6f5c1b1beb8a101216f9a028944467c41d8a
uhonogo <poliesternopo@gmail.com>
2017-04-08 18:54:14 +0300

    nffx

--- js/parallax.js
+++ js/parallax.js
@@ -1,13 +1,6 @@
 $(document).ready(function(argument) {
 	$(window).scroll(function () {
 		var lr = $(this).scrollTop();
-
-		// $(".parallaxImage").css({
-		// 	transform : "translateY(-"+lr/40 +'%)',
-		// 	// '-webkit-transform:' "translateY(-"+lr/40 +'%)',
-  //  //    		'-ms-transform:' "translateY(-"+lr/40 +'%)'
-		// });
-		// $(".parallaxImage").css("-webkit-transform": "translateY(-"+lr/40 +"%)");
 		$(".parallaxImage").css({
 			transform: 'translateY(-'+lr/40 +'%)',
 			MozTransform: 'translateY(-'+lr/40 +'%)',
