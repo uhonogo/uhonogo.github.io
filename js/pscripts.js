8dbef04aeb9181373fd19e6030b6f049508dbd2c
uhonogo <poliesternopo@gmail.com>
2017-08-07 21:22:19 +0300

    ffx

--- js/pscripts.js
+++ js/pscripts.js
@@ -8,24 +8,7 @@ $(window).ready(function() {
         $('.pict').removeClass('lookMore');
     }
 });
-$(window).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3
-        }
-    }
-  });
-});
+
 $(document).ready(function() {
     $('#f-lookMore').click(function(event) {
         $('#f-content').toggleClass('mobileDarkBg');
