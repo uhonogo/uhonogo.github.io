dacee127ef7a16b09d692a010c1fb3403578814c
uhonogo <poliesternopo@gmail.com>
2019-04-08 00:51:04 +0300

    new fix
    carousel fix

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,17 @@
-$(document).ready( function () {
-
-	$('.responsive').slick({
-		dots: true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		arrows: false,
-		responsive: [
-			{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
-});
+$('.responsive').slick({
+	dots: true,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	arrows: false,
+	responsive: [
+	  {
+		breakpoint: 1200,
+		settings: {
+		  slidesToShow: 1,
+		  slidesToScroll: 1
+		}
+	  }
+	]
+  });
